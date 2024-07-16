3abbe1255d039876b3b3acaf896fc2b2e4268bb5
MADPARADOX431 <miguelitoc1@outlook.com>
2024-07-15 23:27:35 -0500

    primer comentario

---
+++ src/main/java/com/candyshop/EcommerceCandyshopApplication.java
@@ -0,0 +1,13 @@
+package com.candyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCandyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCandyshopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/candyshop/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.candyshop.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY="buhdshfagzgsdjasdaskhjdkahjdhsajkhjejejej";
+	public static final String JWT_HEADER="Authorization";
+}
